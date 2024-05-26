76a163113554952085dbe59bb9c27b5a7c425c95
J3MK1 <jadekitagawa.me@gmail.com>
2024-05-26 15:29:36 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('#cpf').mask('000.000.000-00')
+
+    $('#tel').mask('(00) 0000-0000', {
+        placeholder: '(00) 1234-5678'
+    })
+
+    $('#cep').mask('00000-000')
+
+    // $('form').validate({
+    //     rules: {
+    //         firstname: {
+    //             required: true
+    //         },
+    //         lastname: {
+    //             required: true
+    //         },
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         },
+    //         terms: {
+    //             required: true
+    //         }
+    //     },
+    //     submitHandler: function(form) {
+    //         console.log(form);
+    //     },
+    //     invalidHandler: function(event, validator) {
+    //         let fieldInvalid = validator.numberOfInvalids();
+    //         if (fieldInvalid) {
+    //             alert(`There is ${fieldInvalid} field(s) missing.`)
+    //         }
+    //         console.log(fieldInvalid);
+    //     }
+    // })
+})
+
+// Validate plugin não está funcionando
